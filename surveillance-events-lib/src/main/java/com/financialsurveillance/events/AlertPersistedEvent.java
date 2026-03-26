250aa1280f172738ffad6af2eb3f11e3d6c639d9
Vatika1 <vatikaprasad@gmail.com>
2026-03-26 15:38:48 -0400

    Created AlertPersistedEvent.java event

---
+++ surveillance-events-lib/src/main/java/com/financialsurveillance/events/AlertPersistedEvent.java
@@ -0,0 +1,29 @@
+package com.financialsurveillance.events;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlertPersistedEvent {
+
+    private UUID alertId;
+    private String alertTypeId;
+    private String tradeId;
+    private String advisorId;
+    private String ruleId;
+    private String ruleName;
+    private ZonedDateTime createdAt;
+    private ZonedDateTime persistedAt;
+    private JsonNode violationDetails;
+    private AlertSeverity severity;
+    private AlertStatus status;
+}
